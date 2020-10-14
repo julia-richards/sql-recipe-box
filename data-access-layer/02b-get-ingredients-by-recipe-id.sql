2c16eff05eae6e13b10e3ca78b745e03813bc5f8
julia-richards <julia.m.richards@gmail.com>
2020-10-14 13:54:25 -0600

    debugged server nvm issues, completed 2a and 2b to get elements of recipe

--- data-access-layer/02b-get-ingredients-by-recipe-id.sql
+++ data-access-layer/02b-get-ingredients-by-recipe-id.sql
@@ -38,4 +38,7 @@
 -- JOIN weather ON (weather.city = cities.name)
 
 
--- YOUR CODE HERE
+SELECT amount, food_stuff, units_of_measure.name
+FROM ingredients
+JOIN units_of_measure ON (ingredients.unit_of_measure_id = units_of_measure.id)
+WHERE ingredients.recipe_id = $1;
